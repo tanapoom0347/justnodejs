dd8f2be6580a8adfd0875e8ee76607177a01b05a
Tanapoom <tanapoom0347@gmail.com>
2023-08-27 00:22:46 +0700

    u

--- 11/01_read_file/justtest.js
+++ 11/01_read_file/justtest.js
@@ -1,13 +1,20 @@
-// บันทึกข้อมูล Request ได้แก่ วันที่, method, url, ลงใน Log file
+// บันทึกข้อมูล Request ได้แก่ วันที่, url ลงใน log file ในโฟลเดอร์ logs
+// 3 วิธี => 1 Sync , 2 Async (callback) , 3 Promise async await
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((request, response) => {
     const { method, url } = request;
 
-    // เขียน Content = ลงไฟล์
+    try {
+        fs.statSync('logs');
+    } catch (error) {
+        fs.mkdirSync('logs');
+    }
+
     const logContent = `${new Date()} : ${method} : ${url}\n`;
-    fs.writeFileSync('request.log', logContent, { flag: 'a+' });
+    fs.writeFileSync(path.join('logs', 'request.log'), logContent, { flag: 'a+' });
     
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/html; charset=UTF-8');
