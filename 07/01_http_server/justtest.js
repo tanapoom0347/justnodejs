950ab15063d6b137a98f2023def789b56f4a83a7
Tanapoom <tanapoom0347@gmail.com>
2023-08-26 19:03:56 +0700

    u

--- 07/01_http_server/justtest.js
+++ 07/01_http_server/justtest.js
@@ -1,3 +1,14 @@
 // สร้างเว็บฝั่ง Server เพื่อตอบสนองต่อ URL เหล่านี้
 // 1. Homepage -> "/"
 // 2. About page -> "/about"
+const http = require('http');
+
+const  server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    response.end('<h1>น่ารัก</h1>');
+});
+
+server.listen(8888, () => {
+    console.log('Server is start the war');
+});
