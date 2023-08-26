22216ea7c7e8208845167738436e29897a903094
Tanapoom <tanapoom0347@gmail.com>
2023-08-26 22:21:00 +0700

    u

--- 08/01_mysql_intro/justtest.js
+++ 08/01_mysql_intro/justtest.js
@@ -3,7 +3,7 @@
 // 2. About page -> "/about"
 require('dotenv').config();
 const http = require('http');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const { APP_PORT, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_HOSTNAME, MYSQL_PORT, MYSQL_DB } = process.env;
 
@@ -13,24 +13,28 @@ const db = mysql.createConnection(dbConnectionString);
 const  server = http.createServer((request, response) => {
     const { method, url } = request;
 
-    let content = '';
+    // let content = '';
     if (method === 'GET' && url === '/') {
         db.query('select * from todo', (error, results) => {
             if (!!error) {
-                console.log(error);
-                console.log(results);
+                console.log('Has error');
             }
+            console.log(results);
+            const resultsString = results.map(item => `<p>${item.title}</p>`).toString();
+            response.setHeader('Content-Type', 'text/html; charset=UTF-8');
+            response.statusCode = 200;
+            // response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+            response.end('<h1>Homepage น่ารัก</h1>' + resultsString);
         });
-        content = '<h1>Homepage น่ารัก</h1>';
+        // content = '<h1>Homepage น่ารัก</h1>';
     }
     else if (method === 'GET' && url === '/about') {
-        content = '<h1>About น่ารัก</h1>';
+        // content = '<h1>About น่ารัก</h1>';
+        response.setHeader('Content-Type', 'text/html; charset=UTF-8');
+        response.statusCode = 200;
+        // response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+        response.end('<h1>About น่ารัก</h1>');
     }
-
-    response.setHeader('Content-Type', 'text/html; charset=UTF-8');
-    response.statusCode = 200;
-    // response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-    response.end(content);
 });
 
 server.listen(8888, () => {
