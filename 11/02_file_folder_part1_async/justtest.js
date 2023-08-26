16c78f657575839108d547dd83103dd2f5b18752
Tanapoom <tanapoom0347@gmail.com>
2023-08-27 00:53:59 +0700

    u

--- 11/02_file_folder_part1_async/justtest.js
+++ 11/02_file_folder_part1_async/justtest.js
@@ -8,16 +8,34 @@ const server = http.createServer((request, response) => {
     const { method, url } = request;
 
     console.log(`Start ${url}`);
-    try {
-        fs.statSync('logs');
-    } catch (error) {
-        fs.mkdirSync('logs');
-    }
 
+    // Sync
+    // try {
+    //     fs.statSync('logs');
+    // } catch (error) {
+    //     fs.mkdirSync('logs');
+    // }
+
+    // const logContent = `${new Date()} : ${method} : ${url}\n`;
+    // fs.writeFileSync(path.join('logs', 'request.log'), logContent, { flag: 'a+' });
+
+    // Async
     const logContent = `${new Date()} : ${method} : ${url}\n`;
-    fs.writeFileSync(path.join('logs', 'request.log'), logContent, { flag: 'a+' });
+    fs.stat('logs', (error, stats) => {
+        if (!!error) {
+            fs.mkdir('logs', (error1) => {
+                fs.writeFile(path.join('logs', 'request.log'), logContent, { flag: 'a+' }, (error2) => {
+                    console.log(`End ${url}`);
+                });
+            });
+            return;
+        }
+        fs.writeFile(path.join('logs', 'request.log'), logContent, { flag: 'a+' }, (error2) => {
+            console.log(`End ${url}`);
+        });
+    });
     
-    console.log(`End ${url}`);
+
 
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/html; charset=UTF-8');
