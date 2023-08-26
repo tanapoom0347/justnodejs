af7dc8a8ae6148454222c7c9a153b4fc1b967ec9
Tanapoom <tanapoom0347@gmail.com>
2023-08-26 19:18:18 +0700

    u

--- 07/01_http_server/justtest.js
+++ 07/01_http_server/justtest.js
@@ -4,11 +4,22 @@
 const http = require('http');
 
 const  server = http.createServer((request, response) => {
+    const { method, url } = request;
+
+    let content = '';
+    if (method === 'GET' && url === '/') {
+        content = '<h1>Homepage น่ารัก</h1>';
+    }
+    else if (method === 'GET' && url === '/about') {
+        content = '<h1>About น่ารัก</h1>';
+    }
+
+    response.setHeader('Content-Type', 'text/html; charset=UTF-8');
     response.statusCode = 200;
-    response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-    response.end('<h1>น่ารัก</h1>');
+    // response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    response.end(content);
 });
 
 server.listen(8888, () => {
-    console.log('Server is start the war');
+    console.log('Server is start the war hahaha');
 });
