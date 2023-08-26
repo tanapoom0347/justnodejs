d5b0c8f2d1e648c4726dc6b58e0f565a80f75b16
Tanapoom <tanapoom0347@gmail.com>
2023-08-27 01:14:48 +0700

    u

--- 11/03_file_folder_part1_promise/justtest.js
+++ 11/03_file_folder_part1_promise/justtest.js
@@ -1,41 +1,28 @@
 // บันทึกข้อมูล Request ได้แก่ วันที่, url ลงใน log file ในโฟลเดอร์ logs
 // 3 วิธี => 1 Sync , 2 Async (callback) , 3 Promise async await
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
     const { method, url } = request;
 
     console.log(`Start ${url}`);
 
-    // Sync
-    // try {
-    //     fs.statSync('logs');
-    // } catch (error) {
-    //     fs.mkdirSync('logs');
-    // }
-
-    // const logContent = `${new Date()} : ${method} : ${url}\n`;
-    // fs.writeFileSync(path.join('logs', 'request.log'), logContent, { flag: 'a+' });
-
-    // Async
-    const logContent = `${new Date()} : ${method} : ${url}\n`;
-    fs.stat('logs', (error, stats) => {
-        if (!!error) {
-            fs.mkdir('logs', (error1) => {
-                fs.writeFile(path.join('logs', 'request.log'), logContent, { flag: 'a+' }, (error2) => {
-                    console.log(`End ${url}`);
-                });
-            });
-            return;
+    try {
+        await fs.stat('logs');
+    }
+    catch (error) {
+        try {
+            await fs.mkdir('logs');
         }
-        fs.writeFile(path.join('logs', 'request.log'), logContent, { flag: 'a+' }, (error2) => {
-            console.log(`End ${url}`);
-        });
-    });
-    
+        catch (error2) {}
+    }
 
+    const logContent = `${new Date()} : ${method} : ${url}\n`;
+    await fs.writeFile(path.join('logs', 'request.log'), logContent, { flag: 'a+' });
+
+    console.log(`End ${url}`);
 
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/html; charset=UTF-8');
